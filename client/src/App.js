2fa6ae2e900df48a84232a545fc16e655d56218c
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-17 21:34:43 -0400

    setting up client side routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,9 +12,14 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/testing" element={<h1>Test Route</h1>}></Route>
+          <Route path="/" element={<h1>Page Count: {count}</h1>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
