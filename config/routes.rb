10be4d008da7f12d0ef5d61a7ac0042ef6060eb0
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-17 20:39:54 -0400

    react setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # -------------testing-------------
+  get '/hello', to: 'application#hello_world'
 end
