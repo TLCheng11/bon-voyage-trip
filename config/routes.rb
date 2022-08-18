7e075d63309b375ea0dde0b237bff95f0f4b4362
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-17 21:38:55 -0400

    Configured server side route redirect

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  # to redirect to client side routes
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
+
   # -------------testing-------------
   get '/hello', to: 'application#hello_world'
 end
