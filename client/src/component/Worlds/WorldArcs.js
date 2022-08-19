a57e63bafc35f0c5d7315967e966a8793def3e6e
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-19 11:01:27 -0400

    add cloud effect to sign in page globe

--- client/src/component/Worlds/WorldArcs.js
+++ client/src/component/Worlds/WorldArcs.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import Globe from 'react-globe.gl';
+import * as THREE from 'three';
 
 function WorldArcs() {
   const globeEl = useRef();
@@ -18,14 +19,33 @@ function WorldArcs() {
       endLat: (Math.random() - 0.5) * 180,
       endLng: (Math.random() - 0.5) * 360,
       color: [['red', 'yellow', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'yellow', 'blue', 'green'][Math.round(Math.random() * 3)]]
-    })))
+    })));
+
+    // Add clouds sphere
+    const CLOUDS_IMG_URL = 'https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/clouds/clouds.png'; // from https://github.com/turban/webgl-earth
+    const CLOUDS_ALT = 0.004;
+    const CLOUDS_ROTATION_SPEED = -0.006; // deg/frame
+
+    new THREE.TextureLoader().load(CLOUDS_IMG_URL, cloudsTexture => {
+      const clouds = new THREE.Mesh(
+        new THREE.SphereBufferGeometry(globeEl.current.getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
+        new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
+      );
+      globeEl.current.scene().add(clouds);
+
+      (function rotateClouds() {
+        clouds.rotation.y += CLOUDS_ROTATION_SPEED * Math.PI / 180;
+        requestAnimationFrame(rotateClouds);
+      })();
+    });
   }, []);
 
   return (  
-    <div className='fixed -z-10'>
+    <div className='fixed -z-20'>
       <Globe
         ref={globeEl}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         arcsData={arcsData}
         arcColor={'color'}
         arcDashLength={() => Math.random()}
