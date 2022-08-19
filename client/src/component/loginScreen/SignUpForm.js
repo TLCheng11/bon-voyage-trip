4ba1146d589c362ca554d3bfed3c48443c37533f
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-19 11:46:32 -0400

    changed tilwind form configs

--- client/src/component/loginScreen/SignUpForm.js
+++ client/src/component/loginScreen/SignUpForm.js
@@ -18,7 +18,7 @@ function SignUpForm({setSignUp}) {
               <input
                 id="username"
                 name="username"
-                type="username"
+                type="text"
                 autoComplete="username"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
@@ -46,7 +46,7 @@ function SignUpForm({setSignUp}) {
               <input
                 id="comfirm-password"
                 name="comfirm-password"
-                type="comfirm-password"
+                type="password"
                 autoComplete="current-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
