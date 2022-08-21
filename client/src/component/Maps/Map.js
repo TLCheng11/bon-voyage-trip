9d1d73b0b6daf9625e079dec723755391987c04e
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-20 23:11:44 -0400

    added nearby search for hotel, restaurant, sight spot and museum

--- client/src/component/Maps/Map.js
+++ client/src/component/Maps/Map.js
@@ -1,55 +1,61 @@
 
 import { GoogleMap, useJsApiLoader, Marker, MarkerClusterer, InfoWindow } from '@react-google-maps/api';
 import { formatRelative } from 'date-fns';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
-function Map({mapRef, coordinates}) {
+function Map({mapRef, coordinates, nearBy}) {
   // saving the map with a ref so we can have access without re-rendering
   const onMapLoad = useCallback(map => {
     mapRef.current = map
   }, [])
 
   // constants for google maps
-  const [userMarkers, setUserMarker] = useState([])
   const liabraries = ["places"]
   const options = {
     disableDefaultUI: true,
     zoomControl: true,
   }
-
+  
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
     liabraries,
   })
-
+  
   // constants for showing marker info
   const [selectedMarker, setselectedMarker] = useState(null);
+  const iconUrl = {
+    lodging: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    restaurant: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png",
+    tourist_attraction: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/generic_business-71.png",
+    museum: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/museum-71.png",
+  }
 
-  const showMarkers = userMarkers.map(marker => (
-      <Marker 
-        key={marker.time.toISOString()} 
-        position={{lat: marker.lat, lng: marker.lng}}
-        onClick={() => setselectedMarker(marker)}
-      />
-    ))
-  
-  // to fetch nearby hotel through ruby server
-  // useEffect(() => {
-  //   fetch(`/hotel?lat=${coordinates.lat}&lng=${coordinates.lng}&radius=1500&type=restaurant&keyword=cruise&key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`)
-  //   .then(res => res.json())
-  //   .then(console.log)
-  // }, [coordinates]);
+  const showMarkers = nearBy.data ? nearBy.data.map(marker => (
+    <Marker 
+      key={marker.place_id} 
+      position={{lat: marker.geometry.location.lat, lng: marker.geometry.location.lng}}
+      icon={{
+        url: iconUrl[nearBy.type],
+        scaledSize: new window.google.maps.Size(40, 40),
+        origin: new window.google.maps.Point(0, 0),
+        anchor: new window.google.maps.Point(20, 20),
+      }}
+      onClick={() => setselectedMarker(marker)}
+    />
+    )) : null
 
-  const onMapClick = useCallback(e => {
-    setUserMarker(current => [
-      ...current, 
-      {
-        lat: e.latLng.lat(),
-        lng: e.latLng.lng(),
-        time: new Date(),
-      }
-    ])
-  }, [])
+    
+  // const [userMarkers, setUserMarker] = useState([])
+  // const onMapClick = useCallback(e => {
+  //   setUserMarker(current => [
+  //     ...current, 
+  //     {
+  //       lat: e.latLng.lat(),
+  //       lng: e.latLng.lng(),
+  //       time: new Date(),
+  //     }
+  //   ])
+  // }, [])
 
   if (loadError) return <div>Error Loading map!</div>
   if (!isLoaded) return <div>Loading...</div>
@@ -62,16 +68,34 @@ function Map({mapRef, coordinates}) {
         center={coordinates} 
         mapContainerClassName="h-192 w-full"
         options={options}
-        onClick={onMapClick}
-      >
+        // onClick={onMapClick}
+      > 
         {showMarkers}
         {
           selectedMarker ? (
-            <InfoWindow position={{lat: selectedMarker.lat, lng: selectedMarker.lng}} onCloseClick={() => setselectedMarker(null)}>
+            <InfoWindow 
+              position={{lat: selectedMarker.geometry.location.lat, lng: selectedMarker.geometry.location.lng}} 
+              onCloseClick={() => setselectedMarker(null)} 
+              onUnmount={() => setselectedMarker(null)}
+            >
               <div>
-                <p>{selectedMarker.lat}</p>
-                <p>{selectedMarker.lng}</p>
-                <p>{formatRelative(selectedMarker.time, new Date())}</p>
+                <p>{selectedMarker.name}</p>
+                <p>{selectedMarker.vicinity}</p>
+                {
+                  selectedMarker.opening_hours ? (
+                    selectedMarker.opening_hours.open_now ? (<p>Currently Open.</p>) : (<p>Closed</p>)
+                    ) : (null)
+                }
+                {
+                  selectedMarker.rating ? (
+                    <p>Rating: {selectedMarker.rating}</p>
+                    ) : (null)
+                }
+                {
+                  selectedMarker.photos ? (
+                    <a href={selectedMarker.photos[0].html_attributions[0].match(/".*"/)}>view on google map</a>
+                  ) : (null)
+                }
               </div>
             </InfoWindow>
           ) : (null)
--- client/src/component/Maps/MapHolder.js
+++ client/src/component/Maps/MapHolder.js
@@ -1,25 +1,53 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Map from "./Map";
 
 function MapHolder({coordinates}) {
   const mapRef = useRef()
-  const [loadMap, setloadMap] = useState(false)
+  const [loadMap, setLoadMap] = useState(false)
+  const [nearBy, setNearBy] = useState({});
+
+  function searchNearby(type) {
+    if(mapRef.current) {
+      fetch(`/search?lat=${mapRef.current.center.lat()}&lng=${mapRef.current.center.lng()}&radius=1500&type=${type}&key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.status === "OK") {
+          setNearBy({
+            type: type,
+            data: data.results
+          })
+        }
+      })
+    }
+  }
+
+  console.log(nearBy)
 
   return (
     <div>
       <div>
         <div>
-          <button onClick={() => setloadMap(true)}>LoadMap</button>
+          <button onClick={() => setLoadMap(true)}>LoadMap</button>
         </div>
         <div>
           <button onClick={() => console.log([mapRef.current.center.lat(), mapRef.current.center.lng()])}>Map Info...</button>
         </div>
+        <div>
+          <button onClick={() => searchNearby("lodging")}>Nearby Hotel</button>
+        </div>
+        <div>
+          <button onClick={() => searchNearby("restaurant")}>Nearby Restaurants</button>
+        </div>
+        <div>
+          <button onClick={() => searchNearby("tourist_attraction")}>Nearby Sigth Spot</button>
+        </div>
+        <div>
+          <button onClick={() => searchNearby("museum")}>Nearby Museum</button>
+        </div>
       </div>
       {
         loadMap ? (
-          <div>
-            <Map mapRef={mapRef} coordinates={coordinates}/>
-          </div>
+          <Map mapRef={mapRef} coordinates={coordinates} nearBy={nearBy}/>
         ) : (
           null
         )
