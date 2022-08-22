1657dd0ac083f61c861aca3b046d3875e7968f26
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-22 17:03:40 -0400

    add dailyplans page

--- client/src/component/TripDetails/DailyPlans.js
+++ client/src/component/TripDetails/DailyPlans.js
@@ -1,8 +1,14 @@
-function DailyPlans() {
-  
-  return (
-    <div>
+import moment from "moment";
 
+function DailyPlans({dailyPlan, index}) {
+  console.log(dailyPlan)
+  console.log()
+
+  return (
+    <div className="m-5 border border-black p-3 rounded-xl">
+      <h1>Day {index}</h1>
+      <h2 className="w-max">{moment(dailyPlan.day).format("YYYY-MM-DD")}</h2>
+      <h2 className="w-max">{`Destination: ${dailyPlan.city} (${dailyPlan.country})`}</h2>
     </div>
   );
 }
--- client/src/component/TripDetails/TripDetails.js
+++ client/src/component/TripDetails/TripDetails.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import moment from "moment";
+import DailyPlans from './DailyPlans'
 
 function TripDetails() {
   let navigate = useNavigate()
@@ -28,13 +29,20 @@ function TripDetails() {
     }
   }
 
-  const showDailyPlans = trip.daily_plans.forEach(plan => console.log(plan))
+  let i = 0
+  const showDailyPlans = trip.daily_plans.map(plan => {
+    i += 1
+    return <DailyPlans key={plan.id} dailyPlan={plan} index={i}/>
+  })
 
   return (
     <div>
       <h1>{trip.title}</h1>
       <div>{moment(trip.start_date).calendar()} - {moment(trip.end_date).calendar()}</div>
       <button onClick={deleteTrip}>Delete Trip</button>
+      <div className="grid grid-cols-4 gap-4 overflow-auto">
+        {showDailyPlans}
+      </div>
     </div>
   );
 }
